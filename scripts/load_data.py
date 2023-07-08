d857ea0e6612ff8f4d43c17737f309280cdc2159
MAHDI-Imen <mahdi-22@node09.enst.fr>
2023-07-08 23:05:05 +0200

    Train on vendor A

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -85,6 +85,78 @@ def load_3D(data_dir, transform=None):
 
 
 
+def load_vendor_2D(vendor, metadata,transform=None):
+    if transform is None:
+        transform = tio.RescaleIntensity((0, 1))
+
+
+    data_dir = "Data/M&Ms/OpenDataset/"
+    subjects_folders = glob.glob("*/*/", root_dir=data_dir)
+    subject_ids = [s[-7:-1] for s in subjects_folders]
+
+    
+    images = []
+    labels = []
+    for subject_id, folder in tqdm(zip(subject_ids, subjects_folders)):
+        if metadata.loc[subject_id].Vendor == vendor:
+            image_path = os.path.join(data_dir, folder , f"{subject_id}_sa.nii.gz")
+            seg_path = os.path.join(data_dir, folder, f"{subject_id}_sa_gt.nii.gz")
+
+            image = tio.ScalarImage(image_path)
+            seg = tio.LabelMap(seg_path)
+
+            image = transform(image)
+            seg = transform(seg)
+
+            for slice in range(image.shape[-1]):
+                ed_slice = image.data[0, :, :, slice].unsqueeze(0)
+                images.append(ed_slice)
+                es_slice = image.data[1, :, :, slice].unsqueeze(0)
+                images.append(es_slice)
+                ed_seg = seg.data[0, :, :, slice].unsqueeze(0)
+                labels.append(ed_seg)
+                es_seg = seg.data[1, :, :, slice].unsqueeze(0)
+                labels.append(es_seg)
+
+    dataset = TensorDataset(torch.stack(images), torch.stack(labels))
+    n = len(images)
+
+    print(data_dir, 'Dataset size:', n, 'subjects')
+    return dataset 
+
+
+
+def load_vendor_3D(vendor, metadata, transform=None):
+    if transform==None:
+        transform = tio.RescaleIntensity((0,1))
+
+    data_dir = "Data/M&Ms/OpenDataset/"
+    subjects_folders = glob.glob("*/*/", root_dir=data_dir)
+    subject_ids = [s[-7:-1] for s in subjects_folders]
+
+    subjects = []
+    for subject_id, folder in tqdm(zip(subject_ids, subjects_folders)):
+        if metadata.loc[subject_id].Vendor == vendor:
+            image_path = os.path.join(data_dir, folder , f"{subject_id}_sa.nii.gz")
+            seg_path = os.path.join(data_dir, folder, f"{subject_id}_sa_gt.nii.gz")
+
+            image = tio.ScalarImage(image_path)
+            seg = tio.LabelMap(seg_path)
+
+            subject = tio.Subject(
+                image = image,
+                seg = seg,
+                id = subject_id
+            )
+
+            subjects.append(transform(subject))
+    dataset = tio.SubjectsDataset(subjects)
+    n = len(dataset)
+       
+    print(data_dir ,'Dataset size:', n, 'subjects')
+    return dataset
+
+
 def main():
     return 0
 
